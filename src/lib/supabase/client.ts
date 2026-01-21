a42ec0e921a3e57511aece09ed733f45bf15cb10
Chris <jeneekuqtfbruckk@gmail.com>
2026-01-21 15:34:01 +0800

    Update client.ts

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -1,7 +1,7 @@
-import { createClient } from "@supabase/supabase-js";
+import { createClient as createSupabaseClient } from "@supabase/supabase-js";
 
 export const createClient = () =>
-  createClient(
+  createSupabaseClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   );
